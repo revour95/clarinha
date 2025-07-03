b84dc2d35edad4ada72a091719e39435cfc1f175
revour95 <yms4another@gmail.com>
2025-07-02 21:44:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const sprite = document.getElementById('sprite');
 const prompt = document.getElementById('prompt');
 
 const stages = [
-  { width: '80%', text: 'Tá você até que é corajosa, mas não clique de novo (eu que mando aqui mesmo)' },
-  { width: '85%', text: 'Olha, tá bom já viu, não tô pra brincadeira mais (me obedeça)' },
+  { width: '80%', text: 'Tá você até que é corajosa, mas não clique de novo' },
+  { width: '85%', text: 'Olha, tá bom já, não tô pra brincadeira mais' },
   { width: '90%', text: 'Paro' },
   { width: '95%', text: 'Tá de sacanagem' },
-  { width: '100%', text: 'Não clique em mim, peble' },
+  { width: '100%', text: 'Não clique em mim pebleu' },
   { width: '100%', text: null }  // próximo é final
 ];
 
@@ -24,8 +24,8 @@ sprite.addEventListener('click', () => {
     // tela final: substitui todo o conteúdo da .container
     document.querySelector('.container').innerHTML = `
       <img src="polar-bear.gif" alt="Polar Bear GIF" style="width: 70%; height: auto; margin-bottom: 0.5em;" />
-      <p class="final-message">LAYSLAAAAAAA</p>
-      <p>Feliz aniversárioooo, que Deus te abençoe muito pra todo sempre, muitos anos de vida porque eu tenho muito a conversar contigo e não ouse a parar viu, passar meu tempo com você é sempre muito bom então não mude esse seu lado muito divertido que é inegável que tem, um beijão e aproveita seu diaaaa</p>
+      <p class="final-message">Clarinhaaaaaaa</p>
+      <p>te amo S2</p>
     `;
   }
 });
